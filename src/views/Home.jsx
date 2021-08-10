863e7cc78edcbf6bd0d717701ce0d55ef58672a3
Edson J Vargas <edson.jhair@yahoo.com>
2021-08-10 17:16:31 -0400

    Last commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -18,34 +18,10 @@ const Home = () => {
           </form>
         </header>
         <aside className="aside">
-          <img className="image" src={ HeroMobile } srcSet={ `${HeroMobile} 375w, ${HeroDesktop} 1440w` } alt="" />
+          <img className="image" src={ HeroMobile } srcSet={ `${HeroMobile} 991w, ${HeroDesktop} 992w` } alt="" />
         </aside>
       </section>
     </>
-
-    // <div className="container-fluid">
-    //   <nav className="row navbar-container justify-content-center">
-    //     <div className="col-10">
-    //       <img className="logo" src={ Logo } alt="Base Apparel Logo" />
-    //     </div>
-    //   </nav>
-    //   <header className="row hero-container justify-content-center">
-    //     <div className="col-12 col-xl-5 order-2 gx-0">
-    //       <img className="image" src={ HeroMobile } srcset={ `${HeroMobile} 375w, ${HeroDesktop} 1440w` } alt="" />
-    //     </div>
-    //     <div className="col-10 col-xl-7 order-1">
-    //       <h1 className="title text-center mx-auto"><span className="-first-line">We're</span> coming soon</h1>
-    //       <p className="lead text-center">Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
-    //       <form className="form" action="">
-    //         <div>
-    //           <input type="email" name="" id="" className="input -email" placeholder="Email Address" />
-    //         </div>
-    //         <button className="button -submit" type="submit"><i className="icon-arrow"></i></button>
-    //       </form>
-    //     </div>
-    //   </header>
-    // </div>
-
   )
 }
 
